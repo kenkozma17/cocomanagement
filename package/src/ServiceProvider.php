e335f254f6089479b34284b143f7ef4553d4c193
kenkozma17 <kenkozma17@gmail.com>
2025-07-15 21:34:20 +0800

    Forgot to include package

---
+++ package/src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kenkozma17\CocoCMS;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
